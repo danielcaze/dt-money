ed2a82cc49a212dea96d22dcb997026fe1cd5f2c
Daniel Cazé <danielctlima@gmail.com>
2022-05-27 17:31:08 -0300

    global styles done

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+
     :root {
         --background: #F0F2F5;
         --shape: #FFFFFF;
@@ -34,6 +35,15 @@ export const GlobalStyle = createGlobalStyle`
     body {
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: "Poppins", sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
     }
 
     button {
